f9f9fc29bfa48e941783f54d15df96d93530a7a0
flown222 <jangu3384@gmail.com>
2022-08-03 18:36:06 +0900

    ⚡️ ::  AdminUI 클래스 수정
    ⚡️ ::  AdminUI 클래스 수정

--- AdminUI.java
+++ AdminUI.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.lang.Exception;
+import java.util.Scanner;
 
 public class AdminUI
 {	
@@ -65,12 +65,39 @@ public class AdminUI
 
 	public static void changes()    // 잔돈 보유량확인 메소드
 	{
-		System.out.println("changes");
+		System.out.println("현재 보유한 잔돈은");
+		System.out.printf("500원   %d개 입니다.\n", Change.change.get(500));
+		System.out.printf("1000원  %d개 입니다.\n", Change.change.get(1000));
+		System.out.printf("5000원  %d개 입니다.\n", Change.change.get(5000));
+		System.out.printf("10000원 %d개 입니다.\n", Change.change.get(10000));
+		System.out.printf("총액은 %d입니다.\n", Change.sumCal());
 	}
 
 	public static void addChanges()   // 잔돈 추가 메소드
 	{
-		System.out.println("addchanges");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("입력 가능 금액 단위 (500원 1000원 5000원 10000원)");
+		do
+		{
+			System.out.print("금액 단위와 개수를 입력해 주세요. (공백 구분) : ");
+			int unit = sc.nextInt();
+			if ((unit != 500) && (unit != 1000) && (unit != 5000) && (unit != 10000))
+			{
+				System.out.println("잘못된 금액 단위 입니다. 다시 입력 해주세요." + "\n");
+				continue;
+			}
+			int existingCount = Change.change.get(unit);
+			int count = sc.nextInt();
+			if((unit == 500 && count > 100) || (unit != 500 && count > 50))
+			{
+				System.out.println("추가하려는 잔돈의 갯수가 너무 많습니다. 다시 입력해주세요." + "\n");
+				continue;
+			}
+			Change.change.put(unit,count + existingCount);
+			System.out.printf("%d원을 %d개 추가 하였습니다.\n",unit,count);
+			System.out.println(Change.change.get(unit));
+		}
+		while (true);
 	}
 
 	public static void exit()     // 프로그램종료 메소드
@@ -78,6 +105,8 @@ public class AdminUI
 		System.out.println("프로그램 종료!!!");
 		System.exit(0);
 	}
+
+	
 
 	public static void main(String[] args) throws IOException, NumberFormatException
 	{
