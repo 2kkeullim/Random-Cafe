67a52d716173d4e2837cf38067d7f3bb90f7648b
flown222 <jangu3384@gmail.com>
2022-08-03 15:20:06 +0900

    ⚡️ ::  띄어쓰기 수정
    ⚡️ ::  띄어쓰기 수정

--- AdminLogin.java
+++ AdminLogin.java
@@ -7,12 +7,12 @@ public class AdminLogin
 	public void login() throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String id,pw;
+		String id, pw;
 		do
 		{
 			System.out.print("ID 입력 : ");
 			id = br.readLine();
-			if(!AdminList.account.containsKey(id))
+			if (!AdminList.account.containsKey(id))
 				System.out.println("해당 하는 ID가 없습니다.");
 		}
 		while (!AdminList.account.containsKey(id));
@@ -21,7 +21,7 @@ public class AdminLogin
 		{
 			System.out.print("PW 입력 : ");
 			pw = br.readLine();
-			if(!AdminList.account.get(id).equals(pw))
+			if (!AdminList.account.get(id).equals(pw))
 				System.out.println("비밀 번호가 올바르지 않습니다.");
 		}
 		while (!AdminList.account.get(id).equals(pw));
@@ -32,10 +32,7 @@ public class AdminLogin
 			AdminUI.menuSelect();
 			AdminUI.menuRun();
 		}
-		while(true);	
-
-
-		
+		while(true);			
 	}
 
 	public static void main(String[] args) throws IOException
