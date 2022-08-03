ff4e864882b5273f4bd0210f965067d98b7a50f8
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-03 14:21:21 +0900

    📑 :: Member class 생성
    📑 :: Member class 생성

---
+++ Member.java
@@ -0,0 +1,36 @@
+public class Member
+{
+	private String phoneNumber;
+	private int stamp = 0;
+	private int coupon = 0;
+
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public int getStamp()
+	{
+		return stamp;
+	}
+
+	public void setStamp(int stamp)
+	{
+		this.stamp = stamp;
+	}
+	
+	public int getCoupon()
+	{
+		return coupon;
+	}
+
+	public void setCoupon(int coupon)
+	{
+		this.coupon = coupon;
+	}
+}
