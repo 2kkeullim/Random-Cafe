0063be2be222c1abdcf12277aa981346b94c4b9f
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-05 15:32:06 +0900

    📑 :: Cat class 생성
    📑 :: Cat class 생성

---
+++ Cat.java
@@ -0,0 +1,32 @@
+public class Cat
+{
+	public static void open()
+	{
+		System.out.println();
+		System.out.println("           A____A     A____A     A____A                ");
+		System.out.println("          | ·ㅅ· |   | ·ㅅ· |   | ·ㅅ· |               ");
+		System.out.println("          |      |   |　　  |   |      |               ");
+		System.out.println("          | u　u |   | d　u |   | u　u |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("           U￣￣U     U￣￣U     U￣￣U                ");
+		System.out.println();
+	}
+
+	public static void close()
+	{
+		System.out.println();
+		System.out.println("           A____A     A____A     A____A                ");
+		System.out.println("          | · ·  |   | · ·  |   | · ·  |               ");
+		System.out.println("          |  ^   |   |  ^   |   |  ^   |               ");
+		System.out.println("          | u　u |   | u　u |   | u　u |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("          |      |   |      |   |      |               ");
+		System.out.println("           U￣￣U     U￣￣U     U￣￣U                ");
+		System.out.println();
+	}	
+}
