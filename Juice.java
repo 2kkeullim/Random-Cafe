5953cab6851ac13aa21c7fca440f01e2c065bc1e
flown222 <jangu3384@gmail.com>
2022-08-05 17:04:25 +0900

    ⚡️ ::  Juice class 수정
    ⚡️ ::  Juice class 수정

--- Juice.java
+++ Juice.java
@@ -5,11 +5,19 @@ import java.io.IOException;
 public class Juice extends Options
 {
 	public static final int cost = 1500;
+	public static String drinkName[] = {"사과주스" , "오렌지주스", "포도주스" , "망고주스" ,"딸기주스"};
+	public static int drinkPrice[] = {3000, 3500, 3500 , 4000 , 4000};
+
 	
-	public void set() throws IOException
+	public void set(String drinkName , int drinkPrice) throws IOException
 	{
+		System.out.println(Juice.drinkName[DrinkUI.sel-1]+" 을(를) 선택하셨습니다.");
 		option2();
 		Drink dr = new Drink("cold", iceOption, drinkCountOption);
+		dr.setName(drinkName);
+		dr.setPrice(drinkPrice);
 		Cart.vc.add(dr);
+		Cart.overLap(dr);
+		Cart.printVC();
 	}
 }
