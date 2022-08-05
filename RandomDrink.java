2a44d09873064e756099bcf2c92dff6c640d70c3
Neetjun <dudwnsnar@naver.com>
2022-08-06 01:29:52 +0900

    ⚡️ :: RandomDrink-class-modify
    ⚡️ :: RandomDrink-class-modify

--- RandomDrink.java
+++ RandomDrink.java
@@ -3,11 +3,12 @@ import java.util.Random;
 public class RandomDrink
 {
 	static int n;
-	
-	public static void main(String[] args)
+
+	public static int roll()
 	{
 		Random rd = new Random();
 		n = rd.nextInt(5);
-		System.out.println(n);	
+
+		return n;
 	}
 }
