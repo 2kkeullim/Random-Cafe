c68b092cf794a510eeac1c1e11acfc2e6ec8a70f
younggwan <johnbuuk1@gmail.com>
2022-08-05 18:43:37 +0900

    ⚡️ ::  Smoothie class 수정
    ⚡️ ::  Smoothie class 수정

--- Smoothie.java
+++ Smoothie.java
@@ -5,8 +5,8 @@ import java.io.IOException;
 public class Smoothie extends Options
 {
 	public static final int cost = 2500;
-	public static String drinkName[] = {"오렌지스무디" , "청포도스무디", "망고스무디" , "딸기스무디" ,"요거트스무디"};
-	public static int drinkPrice[] = {4000, 4000, 4500 , 4500 , 5000};
+	public static String drinkName[] = {"오렌지스무디" , "청포도스무디", "망고스무디" , "딸기스무디" ,"요거트스무디", "랜덤스무디"};
+	public static int drinkPrice[] = {4000, 4000, 4500 , 4500 , 5000, 4000};
 	
 	public void set(String drinkName , int drinkPrice) throws IOException
 	{
