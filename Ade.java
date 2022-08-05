107e874d844c754962d7579cd24ec3265237f343
flown222 <jangu3384@gmail.com>
2022-08-05 17:05:12 +0900

    ⚡️ ::  Ade class 수정
    ⚡️ ::  Ade class 수정

--- Ade.java
+++ Ade.java
@@ -5,11 +5,19 @@ import java.io.IOException;
 public class Ade extends Options
 {
 	public static final int cost = 1500;
+	public static String drinkName[] = {"레몬에이드" , "유자에이드", "망고에이드" , "자몽에이드" ,"청포도에이드"};
+	public static int drinkPrice[] = {3000, 3000, 4000 , 4000 , 4000};
 	
-	public void set() throws IOException
+	
+	public void set(String drinkName , int drinkPrice) throws IOException
 	{
+		System.out.println(Ade.drinkName[DrinkUI.sel-1]+" 을(를) 선택하셨습니다.");
 		option2();
 		Drink dr = new Drink("cold", iceOption, drinkCountOption);
+		dr.setName(drinkName);
+		dr.setPrice(drinkPrice);
 		Cart.vc.add(dr);
+		Cart.overLap(dr);
+		Cart.printVC();
 	}
 }
