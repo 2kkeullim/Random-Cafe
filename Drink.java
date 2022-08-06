4a38d4741c1b14fecb723226ced98041a37caad4
DongHyeon Choi <105799662+flown222@users.noreply.github.com>
2022-08-06 17:54:27 +0900

    ⚡️ ::  Drink Class 내 변수 초기화 수정
    ⚡️ ::  Drink Class 내 변수 초기화 수정

--- Drink.java
+++ Drink.java
@@ -13,6 +13,8 @@ public class Drink
 		this.hoc = hoc;
 		this.ice = ice;
 		this.count = count;
+		categoryNumber = CategoryUI.sel-1;
+		drinkNumber = DrinkUI.sel-1;
 	}
 
 	public String getName()
