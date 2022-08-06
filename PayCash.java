c01403e1cceb34071489598cc9ea161e540f209a
flown222 <jangu3384@gmail.com>
2022-08-06 16:09:30 +0900

    ⚡️ ::  PayCash  class 수정
    ⚡️ ::  PayCash  class 수정

--- PayCash.java
+++ PayCash.java
@@ -23,19 +23,45 @@ public class PayCash
 		{
 			System.out.print("현금 얼마를 투입하시겠니까? :");
 			temp = Integer.parseInt(br.readLine());
-			if(temp%500 != 0)
-			{
-				System.out.println("다시 입력해 주십시오.");
-				continue;
-			}
-			ReturnChange.pay   = temp;
-			ReturnChange.drink = Cart.totalSum;  
-			ReturnChange.change = ReturnChange.pay-ReturnChange.drink ;
-			ReturnChange.changePrint();		//잔돈반환결과
-			Receipt.receiptDispForCash();
-	
+		if(temp%500 != 0)
+		{
+			System.out.println("다시 입력해 주십시오.");
+			continue;
+		}
+		ReturnChange.pay   = temp;
+		ReturnChange.drink = Cart.totalSum;  
+		ReturnChange.change = ReturnChange.pay-ReturnChange.drink ;
+		ReturnChange.changePrint();		//잔돈반환결과
 		}
 		while (temp%500 != 0);
+	
+		
+		do
+		{
+			System.out.print("스탬프를 적립하시겠습니까? (예 : 1 , 아니오 : 2) : ");
+			temp = Integer.parseInt(br.readLine());
+			if(temp<1 || temp>2)
+				System.out.println("다시 입력해 주십시오.");
+		}
+		while (temp<1 || temp>2);
+		
+		switch(temp)
+		{
+			case 1: UserList.userLogin(); break;
+			case 2: Receipt.receiptDispForCash(); break;
+		}
+		Sales.totalCard += Cart.totalSum;     //카드 총매출 누적합.
+
+		Cart.vc.clear();
+		Cart.totalSum = 0;
+		System.out.println();
+		do
+		{
+			InitialUI.menuDisp();
+			InitialUI.menuSelect();
+			InitialUI.menuRun();
+		}
+		while (true);
 	}
 
 }
