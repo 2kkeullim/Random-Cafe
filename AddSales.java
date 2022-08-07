d08bcdc928c638f348a7a6ccb4070f0727a28c37
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 04:23:23 +0900

    ⚡️ :: AddSales class 수정
    ⚡️ :: AddSales class 수정

--- AddSales.java
+++ AddSales.java
@@ -1,10 +1,10 @@
 public class AddSales
 {
-	public static final int[] COST = {1000 , 2000 , 1500, 2500 ,1000 ,1500};
-	//카테고리별 원가.  각 카테고리 클래스 안에 있는 원가는 삭제하여도 됨 (이곳에서만 쓰임.)
+	public static final int[] COST = {1000, 2000, 1500, 2500, 1000, 1500};
+	//카테고리별 원가. 각 카테고리 클래스 안에 있는 원가는 삭제하여도 됨 (이곳에서만 쓰임.)
 	
 	// 밑의 두 메소드는 관리자 메뉴 1 2 번 항목을 위한 메소드로 영수증 출력에서 이루어짐.
-	public static void addSales()     
+	public static void addSales()
 	{
 		for(int i=0 ; i < Cart.vc.size(); i++)
 		{
