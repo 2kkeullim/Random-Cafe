ad54dcdf9c910a69cd2c1ffcb59d9c8b9ec4d633
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 02:15:23 +0900

    ♻️ :: CategoryUI class 리팩토링 완료
    ♻️ :: CategoryUI class 리팩토링 완료

--- CategoryUI.java
+++ CategoryUI.java
@@ -6,21 +6,27 @@ public class CategoryUI
 {
 	public static int sel = 1;
 
-	public static void category()
+	public static void categoryDisp()
 	{
-		System.out.println("1. coffee");
-		System.out.println("2. non-coffee");
-		System.out.println("3. juice");
-		System.out.println("4. smoothie");
-		System.out.println("5. tea");
-		System.out.println("6. ade");
-		System.out.println("7. 장바구니");
-		System.out.println("8. 뒤로가기");
-		System.out.println("-------------");
-		System.out.print("메뉴를 선택하세요(1 ~ 8) : ");
+		System.out.println("┌────────────────────────────────────────────────────┐");
+		if (InitialUI.sel == 1)
+			System.out.println("│                       [매장]                       │");
+		else if (InitialUI.sel == 2)
+			System.out.println("│                       [포장]                       │");
+		System.out.println("│                                                    │");		
+		System.out.println("│                      1. coffee                     │");
+		System.out.println("│                      2. non-coffee                 │");
+		System.out.println("│                      3. juice                      │");
+		System.out.println("│                      4. smoothie                   │");
+		System.out.println("│                      5. tea                        │");
+		System.out.println("│                      6. ade                        │");
+		System.out.println("│                      7. 장바구니                   │");
+		System.out.println("│                      8. 뒤로가기                   │");
+		System.out.println("└────────────────────────────────────────────────────┘");
+		System.out.print("\n>> 카테고리를 선택하세요 (1 ~ 8) : ");
 	}
 
-	public static void categoryRun() throws IOException
+	public static void categorySelect() throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -29,28 +35,21 @@ public class CategoryUI
 			sel = Integer.parseInt(br.readLine());
 
 			if (sel >= 1 && sel <= 6)
-			{
 				DrinkUI.drinkRun();
-				category();
-			}
 			else if (sel != 7 && sel != 8)
-			{
-				System.out.println("\n다시 입력하세요\n");
-				category();
-			}
-			if (sel == 7)
-			{
-				cart(); 
+				System.out.println("\n다시 입력하세요.\n");
+			else if (sel == 7)
+				Cart.printVC();
+			if (sel == 7 || sel == 8)
 				break;
-			}
+			categoryDisp();
 		}
 		while (sel != 7 && sel != 8);
 	}
 
-	public static void cart() throws IOException
+	public static void categoryRun() throws IOException
 	{
-		Cart.printVC();
+		categoryDisp();
+		categorySelect();
 	}
-
-	
 }
