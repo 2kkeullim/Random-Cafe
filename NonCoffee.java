0ca20fd935820697d8c1365dc4bd8e03a3af7006
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 17:37:15 +0900

    ♻️ :: NonCoffee class 리팩토링 완료
    ♻️ :: NonCoffee class 리팩토링 완료

--- NonCoffee.java
+++ NonCoffee.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class NonCoffee extends Options
@@ -7,9 +5,9 @@ public class NonCoffee extends Options
 	public static String drinkName[] = {"녹차라떼", "초코라떼", "고구마라떼", "곡물라떼", "딸기라떼", "랜덤라떼"};
 	public static int drinkPrice[] = {3000, 3500, 4000, 4000, 4500, 3500};
 
-	public void set(String drinkName , int drinkPrice) throws IOException
+	public void set(String drinkName, int drinkPrice) throws IOException
 	{	
-		System.out.println("\n" + NonCoffee.drinkName[DrinkUI.sel-1]+" 을(를) 선택하셨습니다.\n");
+		System.out.println("\n" + NonCoffee.drinkName[DrinkUI.sel - 1] + " 을(를) 선택하셨습니다.\n");
 		option1();
 
 		Drink dr = new Drink(hotOrColdOption, iceOption, drinkCountOption);
