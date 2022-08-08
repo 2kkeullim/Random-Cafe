935117cb9fb38ac93e97cea1749cafaacd6b54b1
flown222 <jangu3384@gmail.com>
2022-08-09 00:03:51 +0900

    📝 :: 주석 생성
    📝 :: 주석 생성

--- Drink.java
+++ Drink.java
@@ -1,20 +1,20 @@
 public class Drink
 {
-	private String name;
-	private int price;
-	private String hoc;
-	private String ice;
-	private int count;
-	private int categoryNumber;
-	private int drinkNumber;
+	private String name; // 해당 음료의 이름							 ex) 아메리카노
+	private int price;   // 해당 음료의 한잔 가격						 ex) 1500
+	private String hoc;  // option 메소드로 초기화된 hot or cold		 ex) cold
+	private String ice;  // option 메소드로 초기화된 얼음양			     ex) 얼음 많이
+	private int count;   // cupsCount 메소드로 초기화 된 주문 갯수		 ex)  3
+	private int categoryNumber; // 해당 카테고리의 선택번호를 담을 변수	 ex)  0  (밑의 생성자에서 초기화됨.) - Sales 내 addSales 메소드사용을위해서 존재
+	private int drinkNumber;    // 해당 음료의     선택번호를 담을 변수  ex)  0  (밑의 생성자에서 초기화됨.) - 위와 동일
 	private boolean couponUse;
 
-	public Drink(String hoc, String ice, int count)
+	public Drink(String hoc, String ice, int count)  // Option 메소드로 받아온 값을 매개변수로 인스턴스 생성.
 	{
-		this.hoc = hoc;
+		this.hoc = hoc;                        
 		this.ice = ice;
 		this.count = count;
-		categoryNumber = CategoryUI.sel - 1;
+		categoryNumber = CategoryUI.sel - 1; 
 		drinkNumber = DrinkUI.sel - 1;
 	}
 
