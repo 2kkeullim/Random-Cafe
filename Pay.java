71d20f129c980e5b57fe7192d31f835e7d324426
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 11:33:00 +0900

    ♻️ :: Pay class 리팩토링 완료
    ♻️ :: Pay class 리팩토링 완료

--- Pay.java
+++ Pay.java
@@ -4,27 +4,84 @@ import java.io.IOException;
 
 public class Pay
 {
-	public static int sel=1;
+	public static final int card = 1;
+	public static final int cash = 2;
+	public static final int yes = 1;
+	public static final int no = 2;
+	public static int sel = 1;
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
 
 	public static void cashOrCard() throws IOException
 	{
 		do
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println();
-			System.out.print("결제 방식을 선택하여 주십시오. (1: 카드 결제  2: 현금 결제 ) : ");
+			System.out.print(">> 결제 방식을 선택하여 주십시오. (1: 카드 결제,  2: 현금 결제) : ");
 			sel = Integer.parseInt(br.readLine());
-			if(sel<1 || sel>2)
+
+			if (sel < card || sel > cash)
 			{
 				System.out.println("다시 입력해 주십시오.");
 				continue;
 			}
-			switch(sel)
+
+			switch (sel)
 			{
-				case 1: PayCard.cardInsert(); break;
-				case 2: PayCash.cashInsert(); break;
+				case card: PayCard.cardInsert(); break;
+				case cash: PayCash.cashInsert(); break;
 			}
 		}
-		while (sel<1 || sel>2);	
+		while (sel < card || sel > cash);	
+	}
+
+	public static void insert() throws IOException
+	{
+		String str;
+
+		if (sel == card)
+			System.out.println("\n──────────────────── [카드  결제] ────────────────────");
+		else if (sel == cash)
+			System.out.println("\n──────────────────── [현금  결제] ────────────────────");
+
+		do
+		{
+			System.out.print(">> 투입 : Y or y, 뒤로 가기 : Ctrl+z : ");
+			str = br.readLine();
+
+			if (str == null)
+				cashOrCard();
+		}
+		while (!(str.equals("y") || str.equals("Y")));
+	}
+
+	public static void pay(String str) throws IOException
+	{
+		do
+		{
+			System.out.print(">> 스탬프를 적립하시겠습니까? (예 : 1 , 아니오 : 2) : ");
+			sel = Integer.parseInt(br.readLine());
+
+			if (sel < yes || sel > no)
+				System.out.println("다시 입력해 주십시오.");
+		}
+		while (sel < 1 || sel > no);
+		
+		switch (sel)
+		{
+			case yes: UserList.userLogin(); UserList.saveStampCash();
+			case no: Receipt.receiptDispRun(); break;
+		}
+
+		if (str.equals("card"))
+			Sales.totalCard += Cart.totalSum;		// 카드 총매출 누적합
+		else if (str.equals("cash"))
+			Sales.totalCash += Cart.totalSum;		// 현금 총매출 누적합
+		Sales.totalSale += Cart.totalSum;			// 총매출 누적합
+
+		Cart.vc.clear();
+		Cart.totalSum = 0;
+
+		System.out.println();
+
+		Cafe.operate();
 	}
 }
