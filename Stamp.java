e3cf25b29029f2bf749205a3697e8a518b04feb0
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 13:51:12 +0900

    ♻️ :: Stamp class 리팩토링
    ♻️ :: Stamp class 리팩토링

--- Stamp.java
+++ Stamp.java
@@ -19,23 +19,23 @@ public class Stamp extends UserList
 			Cart.couponUseCount = 0;
 		}
 
-		for(int i=0;i < Cart.vc.size();i++)
+		for (int i = 0; i < Cart.vc.size(); i++)
 		{
-			drinkCountForStamp	+= Cart.vc.get(i).getCount(); // 구매자가 구매한 음료 갯수
+			drinkCountForStamp += Cart.vc.get(i).getCount();	// 구매자가 구매한 음료 갯수
 
-			if(Cart.vc.get(i).getCouponUse() == true)
+			if (Cart.vc.get(i).getCouponUse() == true)
 			{
-				drinkCountForStamp ++;
+				drinkCountForStamp++;
 				Cart.vc.get(i).setCouponUse(false);
 			}
 		}
 		
-		stamp += drinkCountForStamp; // 스탬프 갯수
+		stamp += drinkCountForStamp;							// 스탬프 갯수
 		
 		if (stamp >= 10)
 		{
 			userCoupon += (stamp / 10);
-			userStamp = stamp%10;
+			userStamp = stamp % 10;
 		}
 		else
 			userStamp += stamp;
@@ -49,22 +49,21 @@ public class Stamp extends UserList
 
 	public void printStampResult()
 	{
+		System.out.println("\n┌────────────────────────────────────────────────────┐");
+		System.out.println("│            스탬프 적립이 완료되었습니다.           │");
+		System.out.println("└────────────────────────────────────────────────────┘");
+		
 		if (flag == true)
 		{
-			System.out.println("스탬프 적립이 완료되었습니다.");
-			System.out.println("==============================");
-			System.out.println("적립 후 스탬프 수 : " + user.get(dispUserNumber-1).getStamp());
-			System.out.println("적립 후 쿠폰 수   : " + user.get(dispUserNumber-1).getCoupon());
-			System.out.println("==============================");
+			System.out.println("- 적립 후 스탬프 수 : " + user.get(dispUserNumber - 1).getStamp());
+			System.out.println("- 적립 후 쿠폰 수   : " + user.get(dispUserNumber - 1).getCoupon());
 			flag = false;
 		}
 		else
 		{
-			System.out.println("스탬프 적립이 완료되었습니다.");
-			System.out.println("==============================");
-			System.out.println("적립 후 스탬프 수 : " + user.get(user.size()-1).getStamp());
-			System.out.println("적립 후 쿠폰 수   : " + user.get(user.size()-1).getCoupon());
-			System.out.println("==============================");
+			System.out.println("- 적립 후 스탬프 수 : " + user.get(user.size() - 1).getStamp());
+			System.out.println("- 적립 후 쿠폰 수   : " + user.get(user.size() - 1).getCoupon());
 		}
+		System.out.println("──────────────────────────────────────────────────────");
 	}
 }
