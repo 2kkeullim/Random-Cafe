9190db9ae25fdd43e433f700e0fad9551a7fab16
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 17:38:08 +0900

    ♻️ :: RandomDrink class 리팩토링 완료
    ♻️ :: RandomDrink class 리팩토링 완료

--- RandomDrink.java
+++ RandomDrink.java
@@ -2,12 +2,28 @@ import java.util.Random;
 
 public class RandomDrink
 {
-	static int n;
-
 	public static int roll()
 	{
+		int n;
+
 		Random rd = new Random();
 		n = rd.nextInt(5);
 		return n;
+	}
+
+	public static void detectRandom()
+	{
+		for (int i = 0; i < Cart.vc.size(); i++)
+		{	
+			switch (Cart.vc.get(i).getName())
+			{
+				case "랜덤커피"   : Cart.vc.get(i).setName("(R)" + Coffee.drinkName[roll()]);		break;
+				case "랜덤라떼"   : Cart.vc.get(i).setName("(R)" + NonCoffee.drinkName[roll()]);	break;
+				case "랜덤주스"   : Cart.vc.get(i).setName("(R)" + Juice.drinkName[roll()]);		break;
+				case "랜덤스무디" : Cart.vc.get(i).setName("(R)" + Smoothie.drinkName[roll()]);		break;
+				case "랜덤티"     : Cart.vc.get(i).setName("(R)" + Tea.drinkName[roll()]);			break;
+				case "랜덤에이드" : Cart.vc.get(i).setName("(R)" + Ade.drinkName[roll()]);			break;
+			}
+		}
 	}
 }
