3f80c5a428810960ca51f55ea08c052880060316
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 17:36:48 +0900

    ♻️ :: Smoothie class 리팩토링 완료
    ♻️ :: Smoothie class 리팩토링 완료

--- Smoothie.java
+++ Smoothie.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class Smoothie extends Options
@@ -7,9 +5,9 @@ public class Smoothie extends Options
 	public static String drinkName[] = {"오렌지스무디", "청포도스무디", "망고스무디", "딸기스무디", "요거트스무디", "랜덤스무디"};
 	public static int drinkPrice[] = {4000, 4000, 4500, 4500, 5000, 4000};
 	
-	public void set(String drinkName , int drinkPrice) throws IOException
+	public void set(String drinkName, int drinkPrice) throws IOException
 	{
-		System.out.println("\n" + Smoothie.drinkName[DrinkUI.sel-1]+" 을(를) 선택하셨습니다.\n");
+		System.out.println("\n" + Smoothie.drinkName[DrinkUI.sel - 1] + " 을(를) 선택하셨습니다.\n");
 		cupsCount();
 
 		Drink dr = new Drink("cold", " ", drinkCountOption);
