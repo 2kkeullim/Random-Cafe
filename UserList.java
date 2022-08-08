05c480bd911deafd7793609346c07465a6063727
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 11:34:02 +0900

    ♻️ :: UserList class 리팩토링
    ♻️ :: UserList class 리팩토링

--- UserList.java
+++ UserList.java
@@ -10,16 +10,6 @@ public class UserList extends User
 	public static String pn;
 	public static int couponForLogIn; 
 	public static int dispUserNumber;
-	
-	static
-	{
-		user.add(new User("김"));
-		user.add(new User("이"));
-		user.add(new User("박"));
-
-		user.get(1).setCoupon(1);
-		user.get(2).setCoupon(2);
-	}
 
 	public static void userLogin() throws IOException
 	{
@@ -29,56 +19,49 @@ public class UserList extends User
 
 			for (int i = 0; i < user.size(); i++)
 			{
-				if (user.get(i).getPhoneNumber().equals(pn))
+				if (user.get(i).getPhoneNumber().equals(pn)) // 기존 회원
 				{
-					user.get(i).setUserNumber(i+1);
-					dispUserNumber = user.get(i).getUserNumber();
-					sp.userStamp = user.get(i).getStamp();
-					sp.userCoupon = user.get(i).getCoupon();
-					sp.stampCal();
-					user.get(i).setStamp(sp.userStamp);
-					user.get(i).setCoupon(sp.userCoupon);
-						
-					if (user.get(i).getStamp() >= 10)
-					{
-						user.get(i).setStamp(user.get(i).getStamp()%10);
-						user.get(i).setCoupon(user.get(i).getCoupon()+1);
-					}
-					
+					user.get(i).setUserNumber(i + 1);
+					enroll(i);
+
 					flag = true;
 					couponForLogIn = user.get(i).getCoupon();
 					break;
 				}
-
-				else if(!user.get(i).getPhoneNumber().equals(pn) && i == user.size()-1)
+				else if (!user.get(i).getPhoneNumber().equals(pn) && i == user.size() - 1) // 신규 회원
 				{
-					user.get(user.size()-1).setUserNumber(user.size());
-					dispUserNumber = user.get(user.size()-1).getUserNumber();
 					user.add(new User(pn));
-					sp.userStamp = user.get(user.size()-1).getStamp();
-					sp.userCoupon = user.get(user.size()-1).getCoupon();
-					sp.stampCal();
-					user.get(user.size()-1).setStamp(sp.userStamp);
-					user.get(user.size()-1).setCoupon(sp.userCoupon);
-						
-					if (user.get(user.size()-1).getStamp() >= 10)
-					{
-						user.get(user.size()-1).setStamp(user.get(user.size()-1).getStamp()%10);
-						user.get(user.size()-1).setCoupon(user.get(user.size()-1).getCoupon()+1);
-					}
+					enroll(i);
 					
 					break;
 				}
 			}
 	}
 
+	public static void enroll(int i)
+	{
+		user.get(i).setUserNumber(i + 1);
+		dispUserNumber = user.get(i).getUserNumber();
+		sp.userStamp = user.get(i).getStamp();
+		sp.userCoupon = user.get(i).getCoupon();
+		sp.stampCal();
+		user.get(i).setStamp(sp.userStamp);
+		user.get(i).setCoupon(sp.userCoupon);
+			
+		if (user.get(i).getStamp() >= 10)
+		{
+			user.get(i).setStamp(user.get(i).getStamp() % 10);
+			user.get(i).setCoupon(user.get(i).getCoupon() + 1);
+		}	
+	}
+
 	public static void saveStampCard()
 	{
-			sp.printStampResult();
+		sp.printStampResult();
 	}
 
 	public static void saveStampCash()
 	{
-			sp.printStampResult();
+		sp.printStampResult();
 	}
 }
