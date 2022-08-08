0680b5def2f25dfafaf85a6a5d193eccf6d629a1
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 13:50:09 +0900

    ♻️ :: CouponUse class 리팩토링
    ♻️ :: CouponUse class 리팩토링

--- CouponUse.java
+++ CouponUse.java
@@ -5,62 +5,62 @@ import java.util.Scanner;
 
 public class CouponUse
 {
-	static UserList ul = new UserList();
-	static CategoryUI cu = new CategoryUI();
+	private static UserList ul = new UserList();
+	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 	public static void couponRun() throws IOException
 	{
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.println("	쿠폰을 사용하시겠습니까? (Y 또는 y)	 ");
-			System.out.print(" >> " );
-			String temp = br.readLine();
-			
-			if (temp.equals("Y") || temp.equals("y"))
-			{
-				ul.userLogin();	
+		System.out.print("\n>> 쿠폰을 사용하시겠습니까? (Y 또는 y) : ");
+		String temp = br.readLine();
+		
+		if (temp.equals("Y") || temp.equals("y"))
+		{
+			ul.userLogin();	
 
-				if (ul.flag == true)
+			if (ul.flag)
+			{
+				if (ul.couponForLogIn < 1)
 				{
-					if (ul.couponForLogIn < 1)
-					{
-						System.out.println("============================");
-						System.out.println("사용가능한 쿠폰이 없습니다!");
-						System.out.println("============================");
-						return;
-					}
-					else
-					{
-						System.out.println("============================");
-						System.out.println("로그인에 성공하였습니다.");
-						System.out.println("보유 중인 스탬프 수 : " + ul.user.get(ul.dispUserNumber-1).getStamp());
-						System.out.println("보유 중인 쿠폰 수   : " + ul.user.get(ul.dispUserNumber-1).getCoupon());
-						System.out.println("============================");
-						cu.categoryRun();
-					}
+					System.out.println("\n┌────────────────────────────────────────────────────┐");
+					System.out.println("│            사용가능한 쿠폰이 없습니다!!            │");
+					System.out.println("└────────────────────────────────────────────────────┘");
+					return;
 				}
-				else 
+				else
 				{
-					System.out.println("============================");
-					System.out.println("일치하는 ID가 없습니다!");
-					System.out.println("============================");
+					System.out.println("\n┌────────────────────────────────────────────────────┐");
+					System.out.println("│               로그인에 성공하였습니다.             │");
+					System.out.println("└────────────────────────────────────────────────────┘");
+					System.out.println(" - 보유 중인 스탬프 수 : " + ul.user.get(ul.dispUserNumber-1).getStamp());
+					System.out.println(" - 보유 중인 쿠폰 수   : " + ul.user.get(ul.dispUserNumber-1).getCoupon());
+					CategoryUI.categoryRun();
 				}
 			}
+			else 
+			{
+				System.out.println("\n┌────────────────────────────────────────────────────┐");
+				System.out.println("│              일치하는 ID가 없습니다!!              │");
+				System.out.println("└────────────────────────────────────────────────────┘");
+			}
+		}
 	}
 
 	public static void bargenByCoupon() throws IOException
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int temp;
+		int sel;
 		int minusCoupon = ul.user.get(ul.dispUserNumber-1).getCoupon();
-		System.out.println("쿠폰을 사용할 메뉴 번호를 입력해주세요.");
-		System.out.print("메뉴 번호 입력 : " );
-		temp = Integer.parseInt(br.readLine());
-		Sales.totalCoupon += Cart.vc.get(temp-1).getPrice();
-		if(Cart.vc.get(temp-1).getCount() == 1)
-			Cart.vc.get(temp-1).setPrice(0);
-		Cart.vc.get(temp-1).setCount(Cart.vc.get(temp-1).getCount()-1);
+
+		System.out.println(">> 쿠폰을 사용할 메뉴 번호를 입력해주세요.");
+		System.out.print(">> 메뉴 번호 입력 : ");
+		sel = Integer.parseInt(br.readLine());
+
+		Sales.totalCoupon += Cart.vc.get(sel - 1).getPrice();
+
+		if (Cart.vc.get(sel - 1).getCount() == 1)
+			Cart.vc.get(sel - 1).setPrice(0);
+		Cart.vc.get(sel - 1).setCount(Cart.vc.get(sel - 1).getCount() - 1);
 		
 		Cart.couponUseCount++;
-		Cart.vc.get(temp-1).setCouponUse(true);
+		Cart.vc.get(sel - 1).setCouponUse(true);
 	}
 }
