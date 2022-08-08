c225194566997379f3a76bfb735871a1d0a682be
flown222 <jangu3384@gmail.com>
2022-08-08 10:07:32 +0900

    ⚡️ :: Drink class 주석
    ⚡️ :: Drink class 주석

--- Drink.java
+++ Drink.java
@@ -1,21 +1,21 @@
-public class Drink
-{
-	private String name;
-	private int price;
-	private String hoc;
-	private String ice;
-	private int count;
-	private int categoryNumber;
-	private int drinkNumber;
-	private boolean couponUse;
+public class Drink                   // 장바구니 벡터에 담길 인스턴스
+{ 
+	private String name;             // 음료 이름
+	private int price;				 // 음료 한잔 당 가격
+	private String hoc;				 // 뜨거운 or 차가운  택1 (옵션)
+	private String ice;				 // 얼음양 조절  (옵션)
+	private int count;				 // 주문갯수
+	private int categoryNumber;		 // 주문한 음료의 카테고리 넘버 (구분자)
+	private int drinkNumber;         // 주문한 음료의 메뉴넘버 (구분자) 
+	private boolean couponUse;       // 쿠폰사용여부
 
 	public Drink(String hoc, String ice, int count)
 	{
-		this.hoc = hoc;
-		this.ice = ice;
-		this.count = count;
-		categoryNumber = CategoryUI.sel - 1;
-		drinkNumber = DrinkUI.sel - 1;
+		this.hoc = hoc;							  // 온도 옵션 초기화
+		this.ice = ice;							  // 얼음양 옵션 초기화
+		this.count = count;                       // 갯수초기화 
+		categoryNumber = CategoryUI.sel - 1;      // 인스턴스 생성 시 카테고리 넘버 초기화
+		drinkNumber = DrinkUI.sel - 1;            // 인스턴스 생성 시 메뉴넘버 초기화
 	}
 
 	public String getName()
