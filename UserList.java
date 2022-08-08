e386e707590b03bfb4f1ebead5c5dc06dc0b82d3
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-08-08 13:51:21 +0900

    ♻️ :: UserLisg class 리팩토링 완료
    ♻️ :: UserLisg class 리팩토링 완료

--- UserList.java
+++ UserList.java
@@ -11,40 +11,51 @@ public class UserList extends User
 	public static int couponForLogIn; 
 	public static int dispUserNumber;
 
+	static
+	{
+		user.add(new User("김"));
+		user.add(new User("이"));
+		user.add(new User("박"));
+		user.get(1).setCoupon(1);
+		user.get(2).setCoupon(2);
+	}
+
 	public static void userLogin() throws IOException
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.print(">> 번호를 입력하세요 : " );
-		pn = sc.next(); 
+		pn = sc.next();
 
-			for (int i = 0; i < user.size(); i++)
+		for (int i = 0; i < user.size(); i++)
+		{
+			if (user.get(i).getPhoneNumber().equals(pn)) // 기존 회원
 			{
-				if (user.get(i).getPhoneNumber().equals(pn)) // 기존 회원
-				{
-					user.get(i).setUserNumber(i + 1);
-					enroll(i);
+				user.get(i).setUserNumber(i + 1);
+				enroll(i);
 
-					flag = true;
-					couponForLogIn = user.get(i).getCoupon();
-					break;
-				}
-				else if (!user.get(i).getPhoneNumber().equals(pn) && i == user.size() - 1) // 신규 회원
-				{
-					user.add(new User(pn));
-					enroll(i);
-					
-					break;
-				}
+				flag = true;
+				couponForLogIn = user.get(i).getCoupon();
+				break;
 			}
+			else if (!user.get(i).getPhoneNumber().equals(pn) && i == user.size() - 1) // 신규 회원
+			{
+				user.add(new User(pn));
+				enroll(i);
+				
+				break;
+			}
+		}
 	}
 
 	public static void enroll(int i)
 	{
 		user.get(i).setUserNumber(i + 1);
 		dispUserNumber = user.get(i).getUserNumber();
+
 		sp.userStamp = user.get(i).getStamp();
 		sp.userCoupon = user.get(i).getCoupon();
 		sp.stampCal();
+
 		user.get(i).setStamp(sp.userStamp);
 		user.get(i).setCoupon(sp.userCoupon);
 			
@@ -52,7 +63,7 @@ public class UserList extends User
 		{
 			user.get(i).setStamp(user.get(i).getStamp() % 10);
 			user.get(i).setCoupon(user.get(i).getCoupon() + 1);
-		}	
+		}
 	}
 
 	public static void saveStampCard()
