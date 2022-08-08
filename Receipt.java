65c8e22d99730391995fdd2f78e7e978e7ca610a
NeetJun <dudwnsnar@naver.com>
2022-08-08 23:35:22 +0900

    📑 :: caption-add
    📑 :: caption-add

--- Receipt.java
+++ Receipt.java
@@ -8,13 +8,18 @@ import java.time.LocalTime;
 
 public class Receipt
 {
+	// 영수증에 날짜 출력을 하기 위한 변수들
 	static Calendar cal = Calendar.getInstance();
 	static LocalDate nd = LocalDate.now();
 	static LocalTime nt = LocalTime.now();
-
+	
+	//현재시간
 	static int hour = nt.getHour();
+	//현재 분
 	static int minute = nt.getMinute();
+	//현재 초
 	static int second = nt.getSecond();
+	// 강의 시간에 배운 요일 출력하는법 활용
 	static String[] week = {"일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"};
 
 	static String temp;
@@ -33,11 +38,14 @@ public class Receipt
 			else if (Pay.selPay == 2)
 				receiptDispForCash();	// 현금결제 시 현금 영수증
 		}
+		// ??
 		Sales.addSales();
 	}
 
 	public static void receiptDispForCard()
 	{
+		// 마지막으로 랜덤음료를 골랐을 경우
+		// 이를 감지하여 무작위로 음료 이름을 영수증에 출력해줌
 		RandomDrink.detectRandom();
 		System.out.println("\n──────────────────────────────────────────────────────");
 		System.out.println("                    [영   수   증]                   \n");
@@ -46,6 +54,8 @@ public class Receipt
 		System.out.println("──────────────────────────────────────────────────────");
 		for (int i = 0; i < Cart.vc.size(); i++)
 		{
+			// 영수증에서도 장바구니와 마찬가지로 만약 쿠폰을 사용한 메뉴라면 
+			// 가격 계산을 위해 갯수를 -1 시켜줬으므로 출력 갯수를 +1로 표기해줌
 			if (Cart.vc.get(i).getCouponUse() == true)
 				System.out.printf("%2d번 %7s %7s %8s %8d원 %3d개\n", i + 1, Cart.vc.get(i).getName(), Cart.vc.get(i).getHoc(), Cart.vc.get(i).getIce()
 					, Cart.vc.get(i).getPrice() * Cart.vc.get(i).getCount(), Cart.vc.get(i).getCount() + 1);
@@ -59,6 +69,8 @@ public class Receipt
 	public static void receiptDispForCash()
 	{
 		receiptDispForCard();
+		// 카드 영수증 양식을 그대로 가져오고 거기에 거스름돈 양식만 더해줌
+		// pay는 지불한 금액, getChangeSum()은 ReturnChange의 returnChangeSum 변수를 가져오는 메소드
 		System.out.println("──────────────────────────────────────────────────────");
 		System.out.println("지불하신 금액 : " + ReturnChange.pay);
 		System.out.println("거스름돈      : " + ReturnChange.getChangeSum());
